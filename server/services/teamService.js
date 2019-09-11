c49aece56b5fdf78785c8442c11c7e3d5133d3db
Paweł Skowroński <p.skowronski97@o2.pl>
2019-09-11 23:38:55 +0200

    Team service

---
+++ server/services/teamService.js
@@ -0,0 +1,49 @@
+import model from '../models';
+
+const {Team} = model;
+
+async function createTeam({name, leagueId}) {
+    try {
+        return Team.create({name, leagueId});
+    } catch (e) {
+        return null;
+    }
+}
+
+async function updateTeam({id, name, leagueId}) {
+    try {
+        const team = await Team.findByPk(id);
+        if (team) {
+            return team.update({name, leagueId});
+        }
+    } catch (e) {
+        return null;
+    }
+}
+
+async function getAllTeams() {
+    try {
+        return Team.getAll();
+    } catch (e) {
+        return null;
+    }
+}
+
+async function deleteTeam(id) {
+    try {
+        const team = await Team.findByPk(id);
+        if (team) {
+            return team.delete();
+        }
+    } catch (e) {
+        return null;
+    }
+}
+
+
+export default {
+    createTeam,
+    getAllTeams,
+    updateTeam,
+    deleteTeam
+}
