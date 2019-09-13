10b134abab275d6e17fc1b0e2b03d87aad3bd1b8
Paweł Skowroński <p.skowronski97@o2.pl>
2019-09-13 21:46:10 +0200

    Initial client with default packages

---
+++ client/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <h1>Login Page</h1>
+                <b-form>
+                    <b-form-input
+                        type="text"
+                        required
+                        placeholder="Login"
+                    ></b-form-input>
+                    <b-form-input
+                            type="password"
+                            required
+                            placeholder="Password"
+                    ></b-form-input>
+                    <b-button>Login</b-button>
+                </b-form>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+<script>
+export default {
+    name: 'Login',
+    components: {},
+    props: {}
+}
+</script>
