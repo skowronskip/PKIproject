9588c06f27796e92a627a661e6c9dfbf3604dc34
Paweł Skowroński <p.skowronski97@o2.pl>
2019-09-16 18:18:55 +0200

    Update base url

--- client/src/utils/apiRequests.js
+++ client/src/utils/apiRequests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://apipki.pskowron.ski';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 const getConfig = config => {
